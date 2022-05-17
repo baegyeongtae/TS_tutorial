3af0d6f2eec8e51c13d3a0fa78e3313cfc34ce2e
baegyeongtae <bkt584@gmail.com>
2022-05-17 23:14:49 +0900

    제네릭의 장점과 타입 추론에서의 이점

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -15,19 +15,38 @@
 // logText<string>('하이');
 
 
-function logText(text: string | number) {
-    console.log(text);
-    // text.split('').reverse().join('');
-    return text;
-}
+// function logText(text: string): string {
+//    console.log(text);
+//    // text.split('').reverse().join('');
+//    return text;
+//}
+
 
 // function logNumber(num: number) {
 //     console.log(num);
 //     return num;
 // }
 
-const a = logText(10);
-a.split('')
-logText('하이');
+
+// function logText(text: string | number) {
+//     console.log(text);
+//     return text;
+// }
+
+// const a = logText(10);
+// logText('하이');
 // const num = logNumber(10);
 // logText(true);
+
+
+function logText<T> (text: T): T {
+    console.log(text);
+    return text;
+}
+
+const str = logText<string>('abc')
+str.split('')
+const login = logText<boolean>(true)
+
+// logText('a');
+// logText(10);
