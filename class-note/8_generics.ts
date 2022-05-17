c73203e5ae366f30d5b2ffb387a081c5262d06c2
baegyeongtae <bkt584@gmail.com>
2022-05-18 00:49:19 +0900

    keyof로 제네릭의 타입 제한하기

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -98,3 +98,22 @@ function logTextLength<T extends LengthType>(text: T): T {
 logTextLength('a');
 logTextLength(10);
 logTextLength({ length: 10 });
+
+
+
+
+
+// generic 타입 제한 3 - keyof
+interface ShoppingItem {
+    name: string;
+    price: number;
+    stock: number;
+}
+
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T): T {
+    return itemOption;
+}
+
+// getShoppingItemOption(10);
+// getShoppingItemOption<string>('a');
+getShoppingItemOption("name");
