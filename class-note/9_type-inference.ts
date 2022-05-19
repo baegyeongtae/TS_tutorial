89f55da01675a65da200f18aad37d65ff1685903
baegyeongtae <bkt584@gmail.com>
2022-05-19 13:25:39 +0900

    인터페이스와 제네릭을 이용한 타입 추론 방식

--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -1,3 +1,4 @@
+// 타입 추론 기본 1
 let a = 'abc';
 
 function getB(b = 10) {
@@ -6,3 +7,15 @@ function getB(b = 10) {
 }
 
 10 + '10' // 1010
+
+
+// 타입 추론 기본 2
+interface Dropdown<T> {
+    value: T;
+    title: string;
+}
+
+let shoppingItem: Dropdown<string> = { 
+    value: 'abc', 
+    title: 'hello'
+}
