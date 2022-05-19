8ef2ce3cb0c387c2c14e9f556cd829925b6e1be4
baegyeongtae <bkt584@gmail.com>
2022-05-19 22:05:31 +0900

    타입 가드를 위한 예제 소개

---
+++ class-note/11_type-guard.ts
@@ -0,0 +1,23 @@
+interface Developer {
+    name: string;
+    skill: string;
+}
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+function introduce(): Developer | Person {
+    return { name: 'Tony', age: 33, skill: 'Iron Making' }
+}
+let tony = introduce();
+console.log(tony.skill);
+
+if((tony as Developer).skill) {
+    let skill = (tony as Developer).skill;
+    console.log(skill);
+} else if ((tony as Person).age) {
+    let age = (tony as Person).age;
+    console.log(age);
+}
