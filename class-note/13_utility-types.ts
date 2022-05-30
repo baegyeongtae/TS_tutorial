3488738686c5d12555e6a65ab32ac730967b9c11
baegyeongtae <bkt584@gmail.com>
2022-05-31 00:29:42 +0900

    유틸리티 타입 사례 - Partial

--- class-note/13_utility-types.ts
+++ class-note/13_utility-types.ts
@@ -7,7 +7,8 @@ interface Product {
     // something: object;
 }
 
-// 상품 목록을 받아오기 위한 API 함수
+
+// 1. 상품 목록을 받아오기 위한 API 함수
 function fetchProducts(): Promise<Product[]> {
 
 }
@@ -20,10 +21,28 @@ function fetchProducts(): Promise<Product[]> {
 // }
 
 
-//유틸리티 타입
+
+// 2. 특정 상품의 상세 정보를 나타내기 위한 함수
+
+// 유틸리티 타입
 type ShoppingItem = Pick<Product, 'id' | 'name' | 'price'>
 
-
 function displayProductDetail(shoppingItem: ShoppingItem): {
+
+}
+
+
+// 3. 특정 상품 정보를 업데이트(갱신)하는 함수
+
+// interface UpdateProduct {
+//     id?: number;
+//     name?: string;
+//     price?: number;
+//     brand?: string;
+//     stock?: number;
+// }
+
+type UpdateProduct = Partial<Product>;
+function updateProductItem(productItem: Partial<Product>) {
 
 }
